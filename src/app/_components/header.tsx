111c71e7d193299f5308f33e04589e4f436a676b
Wander Amaral <wanderamaral013@gmail.com>
2024-05-29 15:35:30 -0300

    refatoration code

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -3,10 +3,14 @@ import { Button } from "./ui/button";
 
 const Header = () => {
   return (
-    <div className="flex justify-between px-5 pt-6">
-      <h1 className="text-xl font-bold text-red-400 uppercase">fsw foods</h1>
-      <Button size={'icon'} variant={'outline'} className="border-none bg-transparent">
-        <MenuIcon />
+    <div className="flex justify-between pt-6">
+      <h1 className="text-xl font-bold uppercase text-red-400">fsw foods</h1>
+      <Button
+        size={"icon"}
+        variant={"outline"}
+        className="border-none bg-transparent"
+      >
+        <MenuIcon size={30} />
       </Button>
     </div>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,25 @@
+import { ChevronRightIcon } from "lucide-react";
 import CategoryList from "./_components/category-list";
 import Header from "./_components/header";
+import ProductList from "./_components/product-list";
 import Search from "./_components/search";
+import { Button } from "./_components/ui/button";
+import { db } from "./_lib/prisma";
 
-export default function Home() {
+export default async function Home() {
+
+  const products = await db.product.findMany({
+    where: { discountPercentage: { gt: 0 } },
+    take: 10,
+    include: {
+      restaurant: {
+        select: {
+          name: true,
+        },
+      },
+    },
+  });
+
   return (
     <main className="px-5">
       <Header />
@@ -11,6 +28,16 @@ export default function Home() {
       </div>
       <div className="pt-6">
         <CategoryList />
+      </div>
+      <div className="pt-6">
+        <div className="flex items-center justify-between">
+          <h1 className="py-4 text- font-bold">Pedidos recomendados</h1>
+          <Button variant={"link"} className="p-0 h-fit">
+            Ver todos
+            <ChevronRightIcon  size={20}/>
+          </Button>
+        </div>
+        <ProductList products={products}/>
       </div>
     </main>
   );
