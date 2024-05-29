b5860b3d9fdb242077c5e2414147c1b7e62fa244
Wander Amaral <wanderamaral013@gmail.com>
2024-05-29 12:15:37 -0300

    feat: add search components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import Header from "./_components/header";
+import Search from "./_components/search";
 
 export default function Home() {
   return (
     <main className="">
       <Header />
+      <div className="px-5 pt-6">
+        <Search />
+      </div>
     </main>
   );
 }
