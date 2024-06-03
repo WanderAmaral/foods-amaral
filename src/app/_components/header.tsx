98210b643665c24001b8f0f10ff05a0772ac53cc
Wander Amaral <wanderamaral013@gmail.com>
2024-06-03 15:34:10 -0300

    feat: add link in logo

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -1,10 +1,13 @@
 import { MenuIcon } from "lucide-react";
 import { Button } from "./ui/button";
+import Link from "next/link";
 
 const Header = () => {
   return (
     <div className="flex justify-between pt-6">
-      <h1 className="text-xl font-bold uppercase text-red-400">fsw foods</h1>
+      <Link href={"/"}>
+        <h1 className="text-xl font-bold uppercase text-red-400">fsw foods</h1>
+      </Link>
       <Button
         size={"icon"}
         variant={"outline"}
