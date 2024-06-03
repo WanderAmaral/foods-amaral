7ec9f6e7219a7e9b80cf407a180da033c49b4080
Wander Amaral <wanderamaral013@gmail.com>
2024-06-03 17:11:24 -0300

    feat: create ensure method search

--- src/app/restaurants/page.tsx
+++ src/app/restaurants/page.tsx
@@ -9,18 +9,24 @@ import RestaurantItem from "../_components/restaurant-item";
 
 const Restaurants = () => {
   const searchParams = useSearchParams();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
   const [restaurants, setRestaurants] = useState<Restaurant[]>([]);
 
   useEffect(() => {
-    const fetchRestaurants = async () => {
-      const searchFor = searchParams.get("search");
-      if (!searchFor) return;
+    try {
+      setIsLoading(false);
+      const fetchRestaurants = async () => {
+        const searchFor = searchParams.get("search");
+        if (!searchFor) return;
 
-      const foundRestaurants = await searchForRestaurants(searchFor);
-      setRestaurants(foundRestaurants);
-    };
-
-    fetchRestaurants();
+        const foundRestaurants = await searchForRestaurants(searchFor);
+        setRestaurants(foundRestaurants);
+      };
+      fetchRestaurants();
+      setIsLoading(true);
+    } catch (error) {
+      console.log(error);
+    }
   }, [searchParams]);
 
   const searchFor = searchParams.get("search");
@@ -29,11 +35,21 @@ const Restaurants = () => {
     return notFound();
   }
 
+  if (!isLoading) {
+    return;
+  }
+
   return (
     <div className=" space-y-2 px-5">
       <Header />
+      {restaurants.length > 0 ? (
+        <h2 className="py-3 text-lg font-semibold">Restaurantes encontrados</h2>
+      ) : (
+        <h2 className="py-3 text-lg font-semibold">
+          Restaurantes não encontrados
+        </h2>
+      )}
 
-      <h2 className="py-3 text-lg font-semibold">Restaurantes Favoritos</h2>
       <div className=" flex flex-col gap-6">
         {restaurants.map((restaurant) => (
           <RestaurantItem
