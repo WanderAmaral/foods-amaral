8c545ccc696a04425ed764b03ac44a39ee8e54e8
Wander Amaral <wanderamaral013@gmail.com>
2024-06-05 12:35:49 -0300

    feat: add cart total price

--- src/app/_components/cart.tsx
+++ src/app/_components/cart.tsx
@@ -1,16 +1,79 @@
 import { useContext } from "react";
 import { CartContext } from "../_context/cart";
-
 import CartItem from "./cart-item";
+import { Card, CardContent } from "./ui/card";
+import { Separator } from "./ui/separator";
+import { Button } from "./ui/button";
+import { formatCurrency } from "../_helpers/price";
 
 const Cart = () => {
-  const { products } = useContext(CartContext);
+  const { products, totalPrice, totalDiscounts, subtotalPrice } =
+    useContext(CartContext);
 
   return (
-    <div className="mt-4 flex flex-col gap-4">
-      {products.map((product) => (
-        <CartItem product={product} key={product.id} />
-      ))}
+    <div className="flex h-full flex-col justify-between py-5">
+      <div className="space-y-4">
+        {products.map((product) => (
+          <CartItem product={product} key={product.id} />
+        ))}
+      </div>
+      <div className="flex flex-col gap-3">
+        {products.length > 0 && (
+          <Card>
+            <CardContent className="flex flex-col gap-4 p-3">
+              <div className="flex justify-between">
+                <span className=" text-xs text-muted-foreground">Subtotal</span>
+                <p className="text-xs text-[#323232]">
+                  R$:{" "}
+                  {Intl.NumberFormat("pt-BR", {
+                    currency: "BRL",
+                    minimumFractionDigits: 2,
+                  }).format(subtotalPrice)}
+                </p>
+              </div>
+              <Separator />
+              <div className="flex justify-between">
+                <span className=" text-xs text-muted-foreground">Entrega</span>
+                <p className="text-xs text-[#323232]">
+                  {Number(products[0].restaurant.deliveryFee) === 0 ? (
+                    <span className="bg-red-400 font-medium uppercase">
+                      Grátis
+                    </span>
+                  ) : (
+                    formatCurrency(Number(products[0].restaurant.deliveryFee))
+                  )}
+                </p>
+              </div>
+              <Separator />
+              <div className="flex justify-between">
+                <span className=" text-xs text-muted-foreground">
+                  Descontos
+                </span>
+                <p className="text-xs text-[#323232]">
+                  {" "}
+                  - R$:{" "}
+                  {Intl.NumberFormat("pt-BR", {
+                    currency: "BRL",
+                    minimumFractionDigits: 2,
+                  }).format(totalDiscounts)}
+                </p>
+              </div>
+              <Separator />
+              <div className="flex justify-between">
+                <span className=" text-xs text-muted-foreground">Total</span>
+                <p className="text-xs text-[#323232]">
+                  R$:{" "}
+                  {Intl.NumberFormat("pt-BR", {
+                    currency: "BRL",
+                    minimumFractionDigits: 2,
+                  }).format(totalPrice)}
+                </p>
+              </div>
+            </CardContent>
+          </Card>
+        )}
+        <Button className="w-full">Finalizar pedido</Button>
+      </div>
     </div>
   );
 };
