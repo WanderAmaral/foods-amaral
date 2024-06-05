69437efeb07f672c57739a2b2c1efaf0bdfd86ff
Wander Amaral <wanderamaral013@gmail.com>
2024-06-05 12:36:10 -0300

    fix: changed stylizing

--- src/app/_components/cart-item.tsx
+++ src/app/_components/cart-item.tsx
@@ -27,55 +27,69 @@ const CartItem = ({ product }: CartItemProps) => {
   };
 
   return (
-    <div className="flex items-center justify-between px-2">
-      <div className="flex gap-2">
-        <Image
-          src={product.imageUrl}
-          alt={product.name}
-          width={100}
-          height={0}
-          className="rounded-lg object-cover shadow-md"
-        />
+    <div className="flex items-center justify-between">
+      <div className="flex items-center gap-4">
+        {/* PARTE DIREITA (FOTO E NOME) */}
 
-        <div className="flex flex-col gap-2">
-          <h1 className="text-sm">{product.name}</h1>
-          <div className="flex  gap-2">
-            <h3 className="font-bold ">
-              {formatCurrency(calculateProductTotalPrice(product))}
-            </h3>
-            <h2>
-              {product.discountPercentage > 0 && (
-                <span className=" text-xs text-slate-500 line-through">
-                  {formatCurrency(Number(product.price))}
-                </span>
+        <div className="flex h-[77px] w-[77px] items-center justify-center rounded-lg bg-accent lg:h-[120px] lg:w-[120px]">
+          <Image
+            src={product.imageUrl}
+            width={0}
+            height={0}
+            sizes="100vw"
+            alt={product.name}
+            className="h-auto max-h-[70%] w-auto max-w-[80%]"
+          />
+        </div>
+
+        <div className="flex flex-col gap-1 lg:gap-2">
+          <p className="text-xs lg:text-sm">{product.name}</p>
+
+          <div className="flex items-center gap-2">
+            <p className="text-sm font-bold lg:text-base">
+              {formatCurrency(
+                calculateProductTotalPrice(product) * product.quantity,
               )}
-            </h2>
+            </p>
+            {product.discountPercentage > 0 && (
+              <p className="text-xs line-through opacity-75 lg:text-sm">
+                {formatCurrency(Number(product.price) * product.quantity)}
+              </p>
+            )}
           </div>
-          <div className="flex items-center gap-2 text-center">
+
+          <div className="flex items-center gap-1 lg:gap-3">
             <Button
-              onClick={handleDecreaseProduct}
-              size={"sm"}
+              size="icon"
               variant={product.quantity === 1 ? "secondary" : "default"}
+              className="h-8 w-8 lg:h-9 lg:w-9"
+              onClick={handleDecreaseProduct}
             >
-              <ChevronLeftIcon size={15} />
+              <ChevronLeftIcon className="h-4 w-4 lg:h-5 lg:w-5" />
             </Button>
-            <span className="w-4">{product.quantity}</span>
-            <Button onClick={handleIncreaseProduct} size={"sm"}>
-              <ChevronRightIcon size={15} />
+
+            <span className="text-xs lg:text-sm">{product.quantity}</span>
+
+            <Button
+              size="icon"
+              variant="default"
+              className="h-8 w-8 lg:h-9 lg:w-9"
+              onClick={handleIncreaseProduct}
+            >
+              <ChevronRightIcon className="h-4 w-4 lg:h-5 lg:w-5" />
             </Button>
-            {/* ATUALIZAR O VALOR DO PRODUTO BASEADO NA QUANTIDADE */}
           </div>
         </div>
       </div>
-      <div>
-        <Button
-          onClick={handleRemoveProductToCart}
-          size={"icon"}
-          variant={"ghost"}
-        >
-          <Trash2 size={16} />
-        </Button>
-      </div>
+
+      <Button
+        size="icon"
+        variant='secondary'
+        onClick={handleRemoveProductToCart}
+        className="h-8 w-8 lg:h-9 lg:w-9"
+      >
+        <Trash2 className="h-4 w-4 lg:h-5 lg:w-5" />
+      </Button>
     </div>
   );
 };
