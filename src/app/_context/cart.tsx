244d1cc9d3ba14c5a9e00a0b9e2c3a2e8bb0e50b
Wander Amaral <wanderamaral013@gmail.com>
2024-06-07 13:00:16 -0300

    commit

--- src/app/_context/cart.tsx
+++ src/app/_context/cart.tsx
@@ -60,12 +60,14 @@ export const CartProvider = ({ children }: { children: ReactNode }) => {
   }, [products]);
 
   const totalPrice = useMemo(() => {
-    return products.reduce((acc, product) => {
-      return acc + calculateProductTotalPrice(product) * product.quantity;
-    }, 0);
+    return (
+      products.reduce((acc, product) => {
+        return acc + calculateProductTotalPrice(product) * product.quantity;
+      }, 0) + Number(products?.[0]?.restaurant?.deliveryFee)
+    );
   }, [products]);
 
-  const totalDiscounts = subtotalPrice - totalPrice;
+  const totalDiscounts = subtotalPrice - totalPrice + Number(products?.[0]?.restaurant?.deliveryFee);
 
   const decreaseProductToCart = (productId: string) => {
     return setProducts((prev) =>
