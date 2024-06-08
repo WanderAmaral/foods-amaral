222206e9d65efe016deec0a3eb40f9a318035688
Wander Amaral <wanderamaral013@gmail.com>
2024-06-08 18:00:00 -0300

    commit

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -1,8 +1,16 @@
+'use client'
 import { MenuIcon } from "lucide-react";
 import { Button } from "./ui/button";
 import Link from "next/link";
+import { useSession } from "next-auth/react";
 
 const Header = () => {
+
+  const {status, data} = useSession()
+
+  console.log(data)
+  console.log(status)
+
   return (
     <div className="flex justify-between pt-6">
       <Link href={"/"}>
